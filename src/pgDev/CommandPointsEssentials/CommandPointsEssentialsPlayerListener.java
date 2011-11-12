421fa7d59d8c54a5b7f6f65e438c126c37d48c2a
Devil boy <kws512@hotmail.com>
2011-11-12 14:14:21 -0800

    first push with core files

---
+++ src/pgDev/CommandPointsEssentials/CommandPointsEssentialsPlayerListener.java
@@ -0,0 +1,23 @@
+package pgDev.CommandPointsEssentials;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author pgDev
+ */
+public class CommandPointsEssentialsPlayerListener extends PlayerListener {
+    private final CommandPointsEssentials plugin;
+
+    public CommandPointsEssentialsPlayerListener(CommandPointsEssentials instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
+}
+
